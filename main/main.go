ed2d26e0f3dd82770b8fc5ece4ab80b797a27657
shannonpearson <shannon.pearson@goverb.com>
2019-11-05 01:38:14 -0600

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
+// HandleRequest sends an HTTP request to the specified website and sends a text if all is not well
+func HandleRequest() error {
+	targetURL := os.Getenv("TARGET_URL")
+	fmt.Printf("Starting health check for %v", targetURL)
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := sns.New(sess)
+
+	httpResp, httpErr := http.Get(targetURL)
+
+	targetPhoneNumber := os.Getenv("PHONE_NUM")
+
+	// send error if page is down
+	if httpErr != nil {
+		fmt.Println(httpErr)
+
+		// send text
+		params := &sns.PublishInput{
+			Message:     aws.String(httpErr.Error()),
+			PhoneNumber: aws.String(targetPhoneNumber),
+		}
+		resp, err := svc.Publish(params)
+		if err != nil {
+			fmt.Println("Publish error.")
+			if awsErr, ok := err.(awserr.Error); ok {
+				// process SDK error
+				fmt.Printf("AWS Publish error: %v", awsErr.Code())
+			} else {
+				fmt.Printf("Error processing AWS publish error: %v", err.Error())
+			}
+			return nil
+		}
+
+		// Pretty-print the response data.
+		fmt.Println("Publish response: ", resp)
+		return nil
+	}
+	defer httpResp.Body.Close()
+
+	fmt.Println("HTTP response received: ", httpResp.StatusCode, http.StatusText(httpResp.StatusCode))
+
+	expectedResponseCode, convErr := strconv.Atoi(os.Getenv("EXPECTED_RESPONSE_CODE"))
+	if convErr != nil {
+		expectedResponseCode = 200
+	}
+
+	if httpResp.StatusCode != expectedResponseCode {
+		errorString := "Incorrect status code received from " + targetURL + ": " + strconv.Itoa(httpResp.StatusCode)
+		fmt.Println(errorString)
+
+		// send text
+		params := &sns.PublishInput{
+			Message:     aws.String(errorString),
+			PhoneNumber: aws.String(targetPhoneNumber),
+		}
+		resp, err := svc.Publish(params)
+
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				fmt.Printf("AWS Publish error: %v", awsErr.Code())
+			} else {
+				fmt.Printf("Error processing AWS publish error: %v", err.Error())
+			}
+			return nil
+		}
+
+		fmt.Println("Publish response: ", resp)
+		return nil
+	}
+
+	body, readError := ioutil.ReadAll(httpResp.Body)
+	if readError != nil {
+		fmt.Printf("Response body read error: %v", readError)
+		return nil
+	}
+	fmt.Println("HTTP response body: ", body)
+	return nil
+}
+
+func main() {
+	HandleRequest()
+}
